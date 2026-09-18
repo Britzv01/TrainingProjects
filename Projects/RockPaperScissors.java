39db5f6da32d44d7d2235910786f8aafd52c0994
Britzv01 <danzhelsardoncillo2206@gmail.com>
2026-09-18 23:22:55 +0800

    Commit

--- Projects/RockPaperScissors.java
+++ Projects/RockPaperScissors.java
@@ -1,4 +1,3 @@
-import java.util.Locale;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,36 +6,43 @@ public class RockPaperScissors {
         Scanner scan = new Scanner(System.in);
         Random random = new Random();
         String [] computerChoices = {"ROCK", "PAPER", "SCISSORS"};
-        String guess;
-
 
         System.out.println("Rock, Paper, Scissors Game!");
 
         while(true) {
-            int randomValue = random.nextInt(0, 3);
-            String computerChoice = computerChoices[randomValue];
+            String userGuess;
 
-            System.out.println("---------------------------");
-            System.out.println("[Rock/Paper/Scissor]");
-            System.out.print("Enter your move: ");
-            guess = scan.nextLine().toUpperCase();
+            while(true) {
+                System.out.println("---------------------------");
+                System.out.println("[Rock/Paper/Scissor]");
+                System.out.print("Enter your move: ");
+                userGuess = scan.nextLine().toUpperCase();
+
+                if (!userGuess.equals("ROCK") && !userGuess.equals("PAPER") && !userGuess.equals("SCISSORS")){
+                    System.out.println("INVALID CHOICE!");
+                    continue;
+                }
+
+                break;
+            }
+
+            int randomValue = random.nextInt(computerChoices.length);
+            String computerChoice = computerChoices[randomValue];
             System.out.println("Computer Choice: " + computerChoice);
             System.out.println("---------------------------");
 
+            if (userGuess.equals(computerChoice)){
+                System.out.println("Tie!");
+            }
 
-            if((guess.equals("ROCK") && computerChoice.equals("PAPER")) || (guess.equals("PAPER") && computerChoice.equals("SCISSORS")) || (guess.equals("SCISSORS") && computerChoice.equals("ROCK"))) {
+            else if((userGuess.equals("ROCK") && computerChoice.equals("PAPER")) || (userGuess.equals("PAPER") && computerChoice.equals("SCISSORS")) || (userGuess.equals("SCISSORS") && computerChoice.equals("ROCK"))) {
                 System.out.println("You Lose!");
             }
 
-            else if (guess.equals(computerChoice)){
-                System.out.println("Tie!");
-            }
             else {
                 System.out.println("You Win!");
             }
-
             System.out.println("---------------------------");
-
 
             System.out.print("Play Again? [Yes] / [No]: ");
             String replay = scan.nextLine().toUpperCase();
