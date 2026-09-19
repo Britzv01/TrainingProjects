d74b161e06c57d485fa45cf0f24c56f80a1ad32d
Britzv01 <danzhelsardoncillo2206@gmail.com>
2026-09-19 20:18:35 +0800

    Commit

---
+++ Practice/src/StringJoin.java
@@ -0,0 +1,10 @@
+void main(){
+        // Joining Strings
+        System.out.println(String.join("-", "2026", "04", "22"));
+
+        // Joining Arrays
+        String [] items = {"Eggs", "Milk", "Cheese"};
+        System.out.println("The recipe for an omelette: " + String.join(", ", items));
+}
+
+
